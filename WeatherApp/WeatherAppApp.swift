e0b91c4ccb711af63628645d90b3ffa60c69970e
premajyothik <prema.kilaparthi@gmail.com>
2024-12-13 23:40:29 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Premajyothi kilaparti on 13/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
